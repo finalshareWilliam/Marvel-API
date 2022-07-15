68b76b84ae2f988e36b45313ef165cad3ea2b4de
finalshareWilliam <william.h.souza@hotmail.com>
2022-07-15 17:37:54 -0300

    1.0.3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
+import Inicio from './routes/Inicio';
+import Sobre from './routes/Sobre';
 
-
+//
+import Navbar from './componentes/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route to={'/'} element={<Inicio/>}/>
-        <Route to={'Sobre'} element={<Sobre/>}/>
+        <Route path={'/'} element={<Inicio/>}/>
+        <Route path={'Sobre'} element={<Sobre/>}/>
 
       </Routes>
     </Router>
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar(){
+    return(
+        <header>
+            <nav className="Navbar">
+                <ul>
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="sobre">Sobre</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
